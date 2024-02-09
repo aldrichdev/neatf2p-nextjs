985d9440c6f02fe73fd28087ae6680f63f3cc44a
aldrichdev <aldrichdev@gmail.com>
2024-02-08 19:31:37 -0700

    F2P-92 | CSS spacing change

--- src/components/atoms/Callout/Callout.styled.ts
+++ src/components/atoms/Callout/Callout.styled.ts
@@ -5,7 +5,7 @@ export const CalloutContainer = styled('blockquote')<{ variant: string }>(
   ({ variant }) => css`
     margin: 20px 0 0;
     max-width: 100%;
-    background-color: ${variant === 'warning' ? 'rgba(236,148,44, 0.5)' : 'rgba(32, 85, 218, 0.5)'};
+    background-color: ${variant === 'warning' ? 'rgba(236, 148, 44, 0.5)' : 'rgba(32, 85, 218, 0.5)'};
     border-left: 5px solid ${variant === 'warning' ? 'rgb(160, 82, 45)' : '#2055da'};
   `,
 )
