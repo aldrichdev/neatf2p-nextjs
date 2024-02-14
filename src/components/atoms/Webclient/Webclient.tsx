fc1146b70c16021c9cfff9ce587a316f321a8a15
aldrichdev <aldrichdev@gmail.com>
2024-02-14 12:10:45 -0700

    F2P-88 | Extracted webclient host to new environment variable

--- src/components/atoms/Webclient/Webclient.tsx
+++ src/components/atoms/Webclient/Webclient.tsx
@@ -3,10 +3,11 @@ import { WebclientIframe } from './Webclient.styled'
 const Webclient = () => {
   const gameServerHost = process.env.NEXT_PUBLIC_GAME_SERVER_HOST
   const gameServerRsaPublicKey = process.env.NEXT_PUBLIC_GAME_SERVER_RSA_PUBLIC_KEY
+  const webclientServerHost = process.env.NEXT_PUBLIC_WEBCLIENT_SERVER_HOST
 
   return (
     <WebclientIframe
-      src={`http://73.3.138.22:8000/client/index.html#free,${gameServerHost},43494,65537,${gameServerRsaPublicKey},true`}
+      src={`http://${webclientServerHost}:8000/client/index.html#free,${gameServerHost},43494,65537,${gameServerRsaPublicKey},true`}
       height='352px'
       width='513px'
     ></WebclientIframe>
