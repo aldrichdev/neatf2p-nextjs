976091742818faa382f480ed6e3cf981227d9beb
aldrichdev <aldrichdev@gmail.com>
2024-02-14 14:32:53 -0700

    F2P-88 | Updated webclient iframe URL to point to new local HTTPS site

--- src/components/atoms/Webclient/Webclient.tsx
+++ src/components/atoms/Webclient/Webclient.tsx
@@ -3,11 +3,10 @@ import { WebclientIframe } from './Webclient.styled'
 const Webclient = () => {
   const gameServerHost = process.env.NEXT_PUBLIC_GAME_SERVER_HOST
   const gameServerRsaPublicKey = process.env.NEXT_PUBLIC_GAME_SERVER_RSA_PUBLIC_KEY
-  const webclientServerHost = process.env.NEXT_PUBLIC_WEBCLIENT_SERVER_HOST
 
   return (
     <WebclientIframe
-      src={`http://${webclientServerHost}:8000/client/index.html#free,${gameServerHost},43494,65537,${gameServerRsaPublicKey},true`}
+      src={`https://${gameServerHost}/index.html#free,${gameServerHost},43494,65537,${gameServerRsaPublicKey},true`}
       height='352px'
       width='513px'
     ></WebclientIframe>
