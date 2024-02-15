a6687b061e6508ec9033b584ec3915c1af7d8dbd
aldrichdev <aldrichdev@gmail.com>
2024-02-14 17:38:00 -0700

    F2P-88 | Added help button to webclient page, updated Webclient component to accept the url, removed unused env var and eslint ignore

--- src/components/atoms/Webclient/Webclient.tsx
+++ src/components/atoms/Webclient/Webclient.tsx
@@ -1,16 +1,10 @@
 import { WebclientIframe } from './Webclient.styled'
+import { WebclientProps } from './Webclient.types'
 
-const Webclient = () => {
-  const gameServerHost = process.env.NEXT_PUBLIC_GAME_SERVER_HOST
-  const gameServerRsaPublicKey = process.env.NEXT_PUBLIC_GAME_SERVER_RSA_PUBLIC_KEY
+const Webclient = (props: WebclientProps) => {
+  const { url } = props
 
-  return (
-    <WebclientIframe
-      src={`https://${gameServerHost}/index.html#free,${gameServerHost},43494,65537,${gameServerRsaPublicKey},true`}
-      height='352px'
-      width='513px'
-    ></WebclientIframe>
-  )
+  return <WebclientIframe src={url} height='352px' width='513px'></WebclientIframe>
 }
 
 export default Webclient
--- src/pages/webclient/index.tsx
+++ src/pages/webclient/index.tsx
@@ -1,19 +1,25 @@
 import { WebclientBanner } from '@atoms/WebclientBanner'
 import { Webclient } from '@atoms/Webclient'
 import Checkbox from '@mui/material/Checkbox'
-import { FormControlLabel } from '@mui/material'
+import { Button, FormControlLabel } from '@mui/material'
 import { ContentBlock } from '@atoms/ContentBlock'
 import { HideAdsFormGroup } from '@styledPages/Webclient.styled'
 import { useState } from 'react'
 import useAuthentication from '@hooks/useAuthentication'
 import { Spinner } from '@molecules/Spinner'
 import { MustBeAdminBlock } from '@molecules/MustBeAdminBlock'
+import { Modal } from '@molecules/Modal'
+import Link from 'next/link'
 
 const WebclientPage = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [hideAds, setHideAds] = useState(false)
   const [hideRunescapeBanners, setHideRunescapeBanners] = useState(false)
+  const [showHelpModal, setShowHelpModal] = useState(false)
   const user = useAuthentication(setIsLoading)
+  const gameServerHost = process.env.NEXT_PUBLIC_GAME_SERVER_HOST
+  const gameServerRsaPublicKey = process.env.NEXT_PUBLIC_GAME_SERVER_RSA_PUBLIC_KEY
+  const webclientUrl = `https://${gameServerHost}/index.html#free,${gameServerHost},43494,65537,${gameServerRsaPublicKey},true`
 
   const handleHideAdsCheck = () => {
     setHideAds(!hideAds)
@@ -21,6 +27,15 @@ const WebclientPage = () => {
 
   const handleHideRunescapeBannersCheck = () => {
     setHideRunescapeBanners(!hideRunescapeBanners)
+  }
+
+  const handleWebclientNotWorkingButtonClick = () => {
+    setShowHelpModal(true)
+  }
+
+  const handleHelpModalClose = () => {
+    setShowHelpModal(false)
+    document.body.style.overflow = 'unset'
   }
 
   if (isLoading) {
@@ -32,10 +47,15 @@ const WebclientPage = () => {
     return <MustBeAdminBlock textColor='white' />
   }
 
+  if (showHelpModal) {
+    // Prevent scrolling
+    document.body.style.overflow = 'hidden'
+  }
+
   return (
     <ContentBlock>
       {!hideAds && <WebclientBanner bannerType='Ad' bannerPlacement='top' />}
-      <Webclient />
+      <Webclient url={webclientUrl} />
       {!hideRunescapeBanners && <WebclientBanner bannerType='Runescape' bannerPlacement='bottom' />}
       <HideAdsFormGroup>
         <FormControlLabel control={<Checkbox color='success' onChange={handleHideAdsCheck} />} label='Hide Ads' />
@@ -43,7 +63,27 @@ const WebclientPage = () => {
           control={<Checkbox color='success' onChange={handleHideRunescapeBannersCheck} />}
           label='Hide Runescape Banners'
         />
+        <FormControlLabel
+          control={<Button variant='text' onClick={handleWebclientNotWorkingButtonClick} />}
+          label='Webclient Not Working?'
+        />
       </HideAdsFormGroup>
+      <Modal
+        open={showHelpModal}
+        handleClose={handleHelpModalClose}
+        heading='Webclient Not Loading?'
+        body={
+          <p>
+            If the webclient is not loading, you need to accept a privacy warning first.{' '}
+            <Link href={webclientUrl} target='_blank'>
+              Click here
+            </Link>{' '}
+            and you will see a privacy warning. Click the <strong>Advanced</strong> button on the page and then click{' '}
+            <strong>Proceed to...</strong> to accept the privacy issue. The game client should then load. Close the
+            current tab and go back to the Neat webclient and refresh the page, and the client should load.
+          </p>
+        }
+      />
     </ContentBlock>
   )
 }
