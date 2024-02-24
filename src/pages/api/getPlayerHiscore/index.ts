6438d14ed45ab2078d62b1d52a83b056464a02d5
aldrichdev <aldrichdev@gmail.com>
2024-02-23 17:42:28 -0700

    F2P-115 | Removed Attack, Defense and Strength info from Hiscores

--- src/pages/api/getPlayerHiscore/index.ts
+++ src/pages/api/getPlayerHiscore/index.ts
@@ -10,13 +10,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<User>) => {
     return Promise.resolve()
   }
 
-  const query = `SELECT p.username, p.skill_total, ms.attack, e.attack AS 'attackxp', ms.defense, e.defense AS 'defensexp',
-    ms.strength, e.strength AS 'strengthxp', ms.hits, e.hits AS 'hitsxp', ms.ranged, e.ranged AS 'rangedxp', ms.prayer,
-    e.prayer AS 'prayerxp', ms.magic, e.magic AS 'magicxp', ms.cooking, e.cooking AS 'cookingxp', ms.woodcut,
-    e.woodcut AS 'woodcutxp', ms.fishing, e.fishing AS 'fishingxp', ms.firemaking, e.firemaking AS 'firemakingxp', ms.crafting,
-    e.crafting AS 'craftingxp', ms.smithing, e.smithing AS 'smithingxp', ms.mining,
-    e.mining AS 'miningxp' FROM maxstats ms JOIN players p ON p.id = ms.playerID
-    JOIN experience e ON e.playerID = p.id WHERE p.group_id != 0 AND p.banned = 0`
+  const query = `SELECT p.username, p.skill_total, e.attack AS 'attackxp', e.defense AS 'defensexp', e.strength AS 'strengthxp',
+    ms.hits, e.hits AS 'hitsxp', ms.ranged, e.ranged AS 'rangedxp', ms.prayer, e.prayer AS 'prayerxp', ms.magic, 
+    e.magic AS 'magicxp', ms.cooking, e.cooking AS 'cookingxp', ms.woodcut, e.woodcut AS 'woodcutxp', ms.fishing, 
+    e.fishing AS 'fishingxp', ms.firemaking, e.firemaking AS 'firemakingxp', ms.crafting, e.crafting AS 'craftingxp',
+    ms.smithing, e.smithing AS 'smithingxp', ms.mining, e.mining AS 'miningxp' FROM maxstats ms
+    JOIN players p ON p.id = ms.playerID JOIN experience e ON e.playerID = p.id WHERE p.group_id != 0 AND p.banned = 0`
 
   return handleQuery<HiscoreDataRow>('game', query, res)
 }
