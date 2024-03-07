1263d7e2e4b2479aa04aa174050beb850d92264c
aldrichdev <aldrichdev@gmail.com>
2024-03-06 17:57:20 -0700

    Put site into maintenance

--- next.config.js
+++ next.config.js
@@ -17,6 +17,13 @@ const nextConfig = {
       },
     ]
   },
+  redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === '1'
+        ? { source: '/((?!maintenance).*)', destination: '/maintenance.html', permanent: false }
+        : null,
+    ].filter(Boolean)
+  },
 }
 
 module.exports = nextConfig
