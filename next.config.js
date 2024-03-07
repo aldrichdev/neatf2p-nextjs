a7538d6896b0486575b7b4a32cb7a1d2fbf1460f
Paul Aldrich <aldrichdev@gmail.com>
2024-03-06 20:28:33 -0700

    Revert "Put site into maintenance"

--- next.config.js
+++ next.config.js
@@ -17,13 +17,6 @@ const nextConfig = {
       },
     ]
   },
-  redirects() {
-    return [
-      process.env.MAINTENANCE_MODE === '1'
-        ? { source: '/((?!maintenance).*)', destination: '/maintenance.html', permanent: false }
-        : null,
-    ].filter(Boolean)
-  },
 }
 
 module.exports = nextConfig
