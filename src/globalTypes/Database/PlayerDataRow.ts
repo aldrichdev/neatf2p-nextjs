7b24ffa728d8ea47ffe290c46b33119d69c54c5c
aldrichdev <aldrichdev@gmail.com>
2024-04-03 18:18:05 -0600

    F2P-147 | Removed pass and salt columns from getGameAccountsForUser query as the website was doing nothing with them

--- src/globalTypes/Database/PlayerDataRow.ts
+++ src/globalTypes/Database/PlayerDataRow.ts
@@ -3,8 +3,6 @@ export interface PlayerDataRow {
   /** Game account name */
   username: string
   former_name: string
-  pass: string
-  salt: string
   combat: number
   creation_date: number
   login_date: number
