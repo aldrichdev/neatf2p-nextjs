5e5e40248d11a093228e4e1fc466cabd1f0285c4
aldrichdev <aldrichdev@gmail.com>
2025-07-13 12:34:41 -0500

    F2P-209 | Fixed issue with users being able to rename their game accounts to the same name but different casing of another user; kept functionality that allows users to rename their accounts to a different casing of the same name

--- src/pages/api/updateGameAccountUsername/index.ts
+++ src/pages/api/updateGameAccountUsername/index.ts
@@ -14,14 +14,23 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<User>) => {
     return
   }
 
-  // The 2nd line ensures that the case-sensitive new name isn't taken.
+  // The 2nd line ensures that the case-insensitive new name isn't taken by someone else.
   // The 3rd line ensures that the username returned from the search is theirs.
   const query = `UPDATE players SET former_name = ?, username = ? WHERE id = ? AND websiteUserId = ?
-    AND NOT EXISTS (SELECT username FROM players WHERE BINARY username = ?) 
+    AND NOT EXISTS (SELECT username FROM players WHERE username = ? AND id != ?)
       AND ? IN (SELECT username FROM players WHERE websiteUserId = ?)
         AND former_name = ''`
 
-  return handleManipulate('game', query, res, [currentName, newName, accountId, userId, newName, currentName, userId])
+  return handleManipulate('game', query, res, [
+    currentName,
+    newName,
+    accountId,
+    userId,
+    newName,
+    accountId,
+    currentName,
+    userId,
+  ])
 }
 
 export default handler
