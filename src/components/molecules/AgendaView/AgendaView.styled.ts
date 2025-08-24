7db651ab99e166c71ecdbacf8e5babea245d781f
aldrichdev <aldrichdev@gmail.com>
2025-08-24 14:10:52 -0500

    F2P-219 | Replaced desktop event calendar with mobile view so we can show recurring events, removed react-big-calendar library, renamed MobileAgendaView to AgendaView, updated AgendaView to always show recurring events, updated AgendaView to show events in the next 6 months instead of only the next 2 months

--- src/components/molecules/AgendaView/AgendaView.styled.ts
+++ src/components/molecules/AgendaView/AgendaView.styled.ts
@@ -1,44 +1,44 @@
 import { styled } from '@mui/material/styles'
 import { css } from '@mui/system'
 
-export const MobileAgendaViewContainer = styled('div')(
+export const AgendaViewBody = styled('div')(
   ({ theme }) => css`
-    ${theme.breakpoints.up('tablet')} {
-      display: none;
-    }
-  `,
-)
-
-export const MobileAgendaViewBody = styled('div')(
-  () => css`
     margin-top: 40px;
     display: flex;
     flex-direction: column;
     gap: 20px;
+
+    ${theme.breakpoints.up('desktop')} {
+      flex-direction: row;
+    }
   `,
 )
-export const MobileEventCard = styled('div')(
-  () => css`
+export const EventCard = styled('div')(
+  ({ theme }) => css`
     border: 1px solid black;
     font-family: Source Sans Pro;
+
+    ${theme.breakpoints.up('tablet')} {
+      width: 400px;
+    }
   `,
 )
 
-export const MobileEventCardTitle = styled('p')(
+export const EventCardTitle = styled('p')(
   () => css`
     border-bottom: 1px solid black;
     margin: 0;
   `,
 )
 
-export const MobileEventCardBody = styled('div')(
+export const EventCardBody = styled('div')(
   () => css`
     padding: 8px;
     text-align: left;
   `,
 )
 
-export const MobileEventCardBodyLine = styled('p')(
+export const EventCardBodyLine = styled('p')(
   () => css`
     margin: 12px 0;
   `,
---
+++ src/components/molecules/AgendaView/AgendaView.tsx
@@ -0,0 +1,72 @@
+import { AgendaViewProps } from './AgendaView.types'
+import { AgendaViewBody, EventCard, EventCardBody, EventCardBodyLine, EventCardTitle } from './AgendaView.styled'
+import { getPrettyDateString } from '@helpers/date/date'
+import { BodyText } from '@atoms/BodyText'
+import Link from 'next/link'
+import { PageHeading } from '@atoms/PageHeading'
+
+/** An agenda view showing events that are happening in the next 6 months, in addition to recurring events, in boxes. */
+const AgendaView = (props: AgendaViewProps) => {
+  const { events } = props
+  const now = new Date()
+  const sixMonthsFromNow = new Date()
+  sixMonthsFromNow.setMonth(now.getMonth() + 6)
+
+  const sortedAndFilteredEvents = events
+    .filter(event => event.recurring || (new Date(event.start) < sixMonthsFromNow && new Date(event.end) > now))
+    .sort((a, b) => new Date(a.start).getTime() - new Date(b.start).getTime())
+
+  return (
+    <>
+      <PageHeading>Upcoming Events</PageHeading>
+      {sortedAndFilteredEvents?.length > 0 && (
+        <BodyText variant='body' topMargin={0}>
+          Below is a list of events that will be happening on the Neat F2P server in the next 6 months. All event times
+          are shown in your local time zone.
+        </BodyText>
+      )}
+      <AgendaViewBody>
+        {sortedAndFilteredEvents?.length > 0 ? (
+          sortedAndFilteredEvents.map(event => (
+            <EventCard key={event.id}>
+              <EventCardTitle>{event.title}</EventCardTitle>
+              <EventCardBody>
+                {event.location && (
+                  <EventCardBodyLine>
+                    <strong>Where:</strong> {event.location}
+                  </EventCardBodyLine>
+                )}
+                {event.recurring ? (
+                  <EventCardBodyLine>
+                    <strong>When:</strong> <u>Every {event.recursEvery}</u> at{' '}
+                    {new Date(event.start).toLocaleTimeString('en-US')}
+                  </EventCardBodyLine>
+                ) : (
+                  <>
+                    <EventCardBodyLine>
+                      <strong>Starts:</strong> {getPrettyDateString(new Date(event.start))}
+                    </EventCardBodyLine>
+                    <EventCardBodyLine>
+                      <strong>Ends:</strong> {getPrettyDateString(new Date(event.end))}
+                    </EventCardBodyLine>
+                  </>
+                )}
+                {event.resource && (
+                  <EventCardBodyLine>
+                    <Link href={event.resource} target='_blank'>
+                      More info
+                    </Link>
+                  </EventCardBodyLine>
+                )}
+              </EventCardBody>
+            </EventCard>
+          ))
+        ) : (
+          <BodyText variant='body'>No Upcoming Events</BodyText>
+        )}
+      </AgendaViewBody>
+    </>
+  )
+}
+
+export default AgendaView
