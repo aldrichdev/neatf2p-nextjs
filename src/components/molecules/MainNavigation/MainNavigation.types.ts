8476efa84f9f63c4a180bb0ddf0673c331eaff9c
aldrichdev <aldrichdev@gmail.com>
2026-07-26 22:31:25 -0500

    Added external link icon to Price Guide nav item

--- src/components/molecules/MainNavigation/MainNavigation.types.ts
+++ src/components/molecules/MainNavigation/MainNavigation.types.ts
@@ -1,9 +1,11 @@
+import { ReactNode } from 'react'
+
 /** A navigation link that appears under a main navigation item.
  * For now, we will not have more than one level of nesting links.
  */
 export type SubNavigationItem = {
   path: string
-  text: string
+  text: string | ReactNode
   opensInNewTab?: boolean
 }
 
